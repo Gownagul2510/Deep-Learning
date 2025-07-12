0a01d3a38417950ad35d2a64f987ed7137475ce0
Gowtham N <gowthamn2023@gmail.com>
2025-07-12 21:07:01 +0530

    Add files via upload

---
+++ gender.py
@@ -0,0 +1,8 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from sklearn.metrics import classification_report, confusion_matrix
